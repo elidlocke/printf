8810870c518721e55b54a8bae79831c8ba47a1f4
Elizabeth Nenniger <elidlocke@gmail.com>
2018-04-25 17:45:21 -0700

    still trying

--- libft/src/ft_putwstr.c
+++ libft/src/ft_putwstr.c
@@ -6,12 +6,11 @@
 /*   By: enennige <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/25 16:43:05 by enennige          #+#    #+#             */
-/*   Updated: 2018/04/25 17:28:34 by enennige         ###   ########.fr       */
+/*   Updated: 2018/04/25 17:44:25 by enennige         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 /*
 ** The ft_putwstr() function outputs a string to the standard output.
@@ -22,13 +21,10 @@ int		ft_putwstr(wchar_t *ws)
 	int	size;
 	if (ws)
 	{
-		size = 0;
-		while (ws[size] != L'\0')
-		{
-			write(1, &ws[size], sizeof(wchar_t));
-			size++;
-		}
+		size = ft_wstrlen(ws);
+		write(1, ws, (size * sizeof(wchar_t)));
 		return (size);
-	}
+	}	
+
 	return (0);
 }
